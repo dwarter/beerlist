0178ae27d477816209e220b790ab2f0a4f579bd5
Daniel Warter <dansbeerlist@gmail.com>
2011-08-23 08:26:09 -0700

    fuck

--- db/migrate/20110815040528_add_hometown_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddHometownToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :hometown, :string
-  end
-end
--- db/migrate/20110815041309_add_beer_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBeerToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :beer, :string
-  end
-end
